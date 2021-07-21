bf32a9fff57f065f8c5f0ab368e2e0ee827863a8
Maicol Battistini <maicolbattistini@live.it>
2021-07-21 19:31:00 +0200

    style: 💄 Template improvements and fixes

--- src/InstatusController.php
+++ src/InstatusController.php
@@ -10,7 +10,7 @@ use Flarum\Post\Command\PostReply;
 use Flarum\Post\Post;
 use Flarum\User\User;
 use JsonException;
-use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Maicol07\Instatus\Partials\Update;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -41,24 +41,22 @@ class InstatusController implements RequestHandlerInterface
             $update = $incident->updates()->pull(0);
             assert($update instanceof Update);
 
-            $footer = $instatus->isMaintenance ? "This maintenance should last {$incident->duration()} minutes" : "This issue is categorized as {$incident->impact()}";
+            $middle = $instatus->isMaintenance ? "This maintenance should last {$incident->duration()} minutes" : "This issue is categorized as **{$incident->impact()}**";
 
             $discussion = $this->bus->dispatch(
                 new StartDiscussion($actor, [
                     'type' => 'discussion',
                     'attributes' => [
-                        'title' => $incident->name(),
-                        'content' => "{$update->body()}
+                        'title' => "[{$incident->status()}] {$incident->name()}",
+                        'content' => "{$update->markdown()}
 
-Created on {$update->createdAt()->formatLocalized('%A %d %B %Y')}
+$middle
 
-Last edited on {$update->updatedAt()->formatLocalized('%A %d %B %Y')}
+**Affected systems: {$incident->affectedComponents()->pluck('name')->join(', ')}**
 
-$footer
+Created on `{$update->createdAt()->formatLocalized('%c')}` - Last edited on `{$update->updatedAt()->formatLocalized('%c')}`
 
-<b>Affected systems: {$incident->affectedComponents()->pluck('name')->join(', ')}</b>
-
-<a href='https://status.maicol07.it/incident/{$incident->id()}'>Read more on Maicol07 Status</a>"
+[Read more on Maicol07 Status](https://status.maicol07.it/incident/{$incident->id()})"
                     ],
                     'relationships' => [
                         'tags' => [
@@ -75,7 +73,7 @@ $footer
             $discussion->instatus_id = $incident->id();
             $discussion->save();
         } else {
-            $discussion = Discussion::whereFirst('instatus_id', $incident->id());
+            $discussion = Discussion::where('instatus_id', $incident->id())->first();
         }
 
         foreach ($incident->updates()->except(0) as $update) {
@@ -87,16 +85,22 @@ $footer
                             'attributes' => [
                                 'content' => "Current status: {$update->status()}
                             
-                            {$update->body()}
+                            {$update->markdown()}
 
-                            Created on {$update->createdAt()->formatLocalized('%A %d %B %Y')}
-                            Last edited on {$update->updatedAt()->formatLocalized('%A %d %B %Y')}
-
-                            <b>Affected systems: {$incident->affectedComponents()->pluck('name')->join(', ')}</b>"
+                            **Affected systems: {$incident->affectedComponents()->pluck('name')->join(', ')}**
+                            
+                            Created on `{$update->createdAt()->formatLocalized('%c')}` - Last edited on `{$update->updatedAt()->formatLocalized('%c')}`"
                             ]
                         ]
                     ], $ipAddress)
                 );
+                $discussion->title = preg_replace_callback('/\[([A-Z])\w+]/', static function ($matches) use ($incident) {
+                    if ($matches[0] !== $incident->status()) {
+                        return $incident->status();
+                    }
+                    return $matches[0];
+                }, $discussion->title);
+                $discussion->save();
             }
         }
 
@@ -109,6 +113,8 @@ $footer
             );
         }
 
-        return new HtmlResponse('<h1>Hello, world!</h1>');
+        return new JsonResponse([
+            'status' => '204'
+        ], 204);
     }
 }
